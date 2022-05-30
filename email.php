826225696c4cacdf2aae9743f4f117e52837ad8f
unknown <ierfan735@gmail.com>
2022-05-30 08:06:45 +0700

    email commit

--- email.php
+++ email.php
@@ -1,36 +1,55 @@
-
 <?php
-$errors = '';
-$myemail = 'ierfan735@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  ||
-empty($_POST['email']) ||
-empty($_POST['phone']) ||
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
+require 'vendor/autoload.php';
+use phpmailer\phpmailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+//Create an instance; passing `true` enables exceptions
+
+if(isset($_POST['submit'])){
+$mail = new PHPMailer(true);
 $name = $_POST['name'];
-$email_address = $_POST['email'];
+$email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'fileIerfan38@gmail.com';                     //SMTP username
+    $mail->Password   = 'fahrud234';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-if( empty($errors))
-{
-$to = $myemail;
-$email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Email: $email_address\n Message \n $message";
-$headers = "From: $myemail\n";
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-//redirect to the 'thank you' page
-header('Location: https://ierfan19.github.io/');
+    //Recipients
+    $mail->setFrom('fileIerfan38@gmail.com', 'portofolio');
+    $mail->addAddress($email, $name);     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('fileIerfan38@gmail.com', 'portofolio');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+
+    if($mail->send()){
+        
+    echo 'Message has been sent';
+    }
+    else{
+        
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+} else{
+    echo "gagal kirim";
 }
 ?>
---
+++ kirim.php
@@ -0,0 +1,54 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once "library/PHPMailer.php";
+require_once "library/Exception.php";
+require_once "library/OAuth.php";
+require_once "library/POP3.php";
+require_once "library/SMTP.php";
+ 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	$mail = new PHPMailer;
+ 
+	//Enable SMTP debugging. 
+	$mail->SMTPDebug = 3;                               
+	//Set PHPMailer to use SMTP.
+	$mail->isSMTP();            
+	//Set SMTP host name                          
+	$mail->Host = "tls://smtp.gmail.com"; //host mail server
+	//Set this to true if SMTP host requires authentication to send email
+	$mail->SMTPAuth = true;                          
+	//Provide username and password     
+	$mail->Username = "fileierfan38@gmail.com";   //nama-email smtp          
+	$mail->Password = "fahrud234";           //password email smtp
+	//If SMTP requires TLS encryption then set it
+	$mail->SMTPSecure = "tls";                           
+	//Set TCP port to connect to 
+	$mail->Port = 587;                                   
+ 
+	$mail->From = $email; //email pengirim
+	$mail->FromName = $name; //nama pengirim
+ 
+	 $mail->addAddress($email, $name); //email penerima
+ 
+	$mail->isHTML(true);
+ 
+	$mail->Subject = $phone; //subject
+    $mail->Body    = $message; //isi email
+        $mail->AltBody = "PHP mailer"; //body email (optional)
+ 
+	if(!$mail->send()) 
+	{
+	    echo "Mailer Error: " . $mail->ErrorInfo;
+	} 
+	else 
+	{
+	    echo "Message has been sent successfully";
+	}
+
+?>
